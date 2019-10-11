341212bbb8ae35cbd8bb707b0b3323e6e3ae5952
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-10-11 14:19:27 +0900

    Initial Commit

---
+++ simpleGeoCoding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simpleGeoCoding
+//
+//  Created by D7702_09 on 2019. 10. 11..
+//  Copyright © 2019년 csd5766. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
